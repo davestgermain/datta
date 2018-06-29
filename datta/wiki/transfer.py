9322492a6b47945902c32de20032f6384d05265e
Dave St.Germain <dcs@adullmoment.com>
2018-06-28 23:16:46 -0400

    better transfer script

--- datta/wiki/transfer.py
+++ datta/wiki/transfer.py
@@ -6,36 +6,50 @@ import datetime
 import six
 import json
 
-
-
-mstore = hatta.storage.WikiStorage(sys.argv[1])
-dsn = 'lmdb:///Users/dcs/lmdbfs/'
-# dsn = 'lmdb:///tmp/wiki/'
-dbstore = dbstorage.WikiStorage(dsn)
-dbstore.set_wiki('om.paragate.club')
-
-history = list(mstore.history())
-history.reverse()
-# tz = datetime.timezone(datetime.timedelta(hours=0))
 class utc(datetime.tzinfo):
     def utcoffset(self, x):
         return None
 
-for title, rev, dt, author, comment in history:
-    content = mstore.page_revision(title, rev)
-    ts = dt.replace(tzinfo=utc())
-    # print(ts)
-    # continue
-    if rev == -1:
-        six.print_('\tDELETE')
-        dbstore.delete_page(title, author, comment, ts=ts)
-    else:
-        dbstore.save_data(title, content, author=author, comment=comment, ts=ts, new=rev==0)
-    six.print_(title, rev)
+
+def transfer_wiki(dsn, pages_dir, domain, site_name):
+
+    mstore = hatta.storage.WikiStorage(pages_dir)
+    dbstore = dbstorage.WikiStorage(dsn)
+    dbstore.set_wiki(domain)
+
+    conf = {u'PAGE_PATH': domain, u'SITE_NAME': site_name}
+    with dbstore.fs.open(u'/.config/wiki/%s' % domain, owner=u'root', mode=u'w') as fp:
+        fp.write(json.dumps(conf))
+
+    dbstore.fs.set_perm(u'/.config/wiki/', u'*', u'r')
+
+    history = list(mstore.history())
+    history.reverse()
+    # tz = datetime.timezone(datetime.timedelta(hours=0))
+
+    for title, rev, dt, author, comment in history:
+        content = mstore.page_revision(title, rev)
+        ts = dt.replace(tzinfo=utc())
+        # print(ts)
+        # continue
+        if rev == -1:
+            six.print_('\tDELETE')
+            dbstore.delete_page(title, author, comment, ts=ts)
+        else:
+            dbstore.save_data(title, content, author=author, comment=comment, ts=ts, new=rev==0)
+        six.print_(title, rev)
+
+if __name__ == '__main__':
+    import argparse
 
 
-conf = {u'PAGE_PATH': u'om.paragate.club', u'SITE_NAME': u'false-dilemma'}
-with dbstore.fs.open(u'/.config/wiki/localhost:8080', owner=u'root', mode=u'w') as fp:
-    fp.write(json.dumps(conf))
+    parser = argparse.ArgumentParser(prog='datta.wiki.transfer', description='transfer hatta pages')
+    parser.add_argument('-d', default='fdb', dest='dsn', help='DSN for file manager')
+    parser.add_argument('-p', dest='page_dir', help='hatta pages', required=True)
+    parser.add_argument('-D', dest='domain', help='domain name', type=six.text_type)
+    parser.add_argument('-s', dest='site', help='site name', type=six.text_type)
+    
+    args = parser.parse_args()
+    # six.print_(args)
+    transfer_wiki(args.dsn, args.page_dir, args.domain, args.site)
 
-dbstore.fs.set_perm(u'/.config/wiki/', u'*', u'r')
