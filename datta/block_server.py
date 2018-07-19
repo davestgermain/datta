76d2770ff6c9cfefa768ea7a10bdc86fa7f47dd2
Dave St.Germain <dcs@adullmoment.com>
2018-07-19 16:02:54 -0400

    that's what i want

--- datta/block_server.py
+++ datta/block_server.py
@@ -220,6 +220,6 @@ class TrioBlockServer(BaseBlockServer):
 
 if __name__ == '__main__':
     import sys
-    server = AsyncioBlockServer(sys.argv[1], host='0.0.0.0')
+    server = AsyncioBlockServer(sys.argv[1], host='')
     server.start()
 
